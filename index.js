b8332bc896039b630ac7e9210e2c03627f9981a5
Ritesh Saini <36216813+heyitsritesh@users.noreply.github.com>
2024-06-08 13:06:42 +0530

    Added initial JS logic
    - Created query selectors
    - Written operator functions
    - Declared variables

---
+++ index.js
@@ -0,0 +1,42 @@
+// Query Selectors
+
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const zero = document.querySelector('#zero');
+const plus = document.querySelector('#plus');
+const minus = document.querySelector('#minus');
+const multiply = document.querySelector('#multiply');
+const divide = document.querySelector('#divide');
+const equals = document.querySelector('#equals');
+const dot = document.querySelector('#dot');
+const clear = document.querySelector('#clear');
+const display = document.querySelector('#display');
+
+// Operators
+
+function addition(x, y) {
+    return x + y;
+}
+
+function subtraction(x, y) {
+    return x - y;
+}
+
+function multiplication(x, y) {
+    return x * y;
+}
+
+function division(x, y) {
+    return x / y;
+}
+
+// Variables
+
+let firstNum, secondNum, operator;
