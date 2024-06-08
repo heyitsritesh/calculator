4d9f7bc0891f13ad2e1d4615a6f52e9642c4f2d4
Ritesh Saini <36216813+heyitsritesh@users.noreply.github.com>
2024-06-08 13:50:16 +0530

    Added event listeners for numbers and dot

--- index.js
+++ index.js
@@ -40,3 +40,63 @@ function division(x, y) {
 // Variables
 
 let firstNum, secondNum, operator;
+
+// Event Listeners
+
+function displayCondition() {
+    if (display.textContent == 0 && display.textContent != '0.')
+        display.textContent = '';
+}
+
+one.addEventListener('click', () => {
+    displayCondition();
+    display.textContent += 1;
+});
+
+two.addEventListener('click', () => {
+    displayCondition();
+    display.textContent += 2;
+});
+
+three.addEventListener('click', () => {
+    displayCondition();
+    display.textContent += 3;
+});
+
+four.addEventListener('click', () => {
+    displayCondition();
+    display.textContent += 4;
+});
+
+five.addEventListener('click', () => {
+    displayCondition();
+    display.textContent += 5;
+});
+
+six.addEventListener('click', () => {
+    displayCondition();
+    display.textContent += 6;
+});
+
+seven.addEventListener('click', () => {
+    displayCondition();
+    display.textContent += 7;
+});
+
+eight.addEventListener('click', () => {
+    displayCondition();
+    display.textContent += 8;
+});
+
+nine.addEventListener('click', () => {
+    displayCondition();
+    display.textContent += 9;
+});
+
+zero.addEventListener('click', () => {
+    if (display.textContent != 0) display.textContent += 0;
+});
+
+dot.addEventListener('click', () => {
+    if (!display.textContent.includes('.')) display.textContent += '.';
+});
